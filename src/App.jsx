e7fd2e3d09ef1821453213575eb20929137abd4f
Tony Nguyen <nhattruong1811@gmail.com>
2023-05-05 09:19:06 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Link, Route, Routes } from "react-router-dom";
+
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+
+  return (
+    <>
+      <h1>Test Browser & Hash Router</h1>
+
+      <nav>
+        <Link to="/" replace>Home</Link> | 
+        <Link to="/contact">Contact</Link> |
+        <Link to="/About">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
