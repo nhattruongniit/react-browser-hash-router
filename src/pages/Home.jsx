2f5f1fd0d11c655295b7b7e6b9ebcf22c20f74e6
Tony Nguyen <nhattruong1811@gmail.com>
2023-10-28 14:48:06 +0700

    abc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,6 @@
-// import { PianoLayout } from "@ama-ecosystem/theme";
-import { Piano } from "@ama-ecosystem/theme";
-
 function Home() {
   return (
     <>
-    <Piano />
       <div>Home</div>
     </>
   )
