dfb5402c9fd38b8671e3b9a2e9ce87a9f26f92b3
tonynguyen <tonyjsfreelancer@gmail.com>
2024-04-20 11:39:45 +0700

    test brwoser

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 )
+
+// browser route + hash route: https://github.com/nhattruongniit/react-browser-hash-router
