e2fc09f70fdaf56ab2eea19299ae83df7daf565a
Tony Nguyen <tonynguyen@192.168.0.154>
2025-04-12 20:17:55 +0700

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
 )
 
 // browser route + hash route: https://github.com/nhattruongniit/react-browser-hash-router
+
+// 1. add event scroll window -> window.innerheight >= element.offsetHeight -> history.replace(xxxx, xxxx#elementId)
